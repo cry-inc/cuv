939e334578ea23fa9810282a43da971724b7813b
cry-inc <cry-inc@users.noreply.github.com>
2024-07-25 21:56:53 +0200

    Added benchmarks for high level Rust interface

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,5 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use cuv::{create_lut, pack, unpack};
+use cuv::{create_lut, pack, unpack, CompUnitVec};
 
 fn create_lut_benchmark(c: &mut Criterion) {
     c.bench_function("create_lut()", |b| {
@@ -29,10 +29,30 @@ fn unpack_benchmark(c: &mut Criterion) {
     });
 }
 
+fn rustified_pack_benchmark(c: &mut Criterion) {
+    c.bench_function("rustified pack(1,0,0)", |b| {
+        b.iter(|| {
+            let vec = CompUnitVec::new(black_box(1.0), black_box(0.0), black_box(0.0));
+            assert_eq!(vec.get_u16(), 255);
+        })
+    });
+}
+
+fn rustified_unpack_benchmark(c: &mut Criterion) {
+    c.bench_function("rustified unpack(255)", |b| {
+        b.iter(|| {
+            let vec = CompUnitVec::from_u16(black_box(255));
+            assert_eq!(vec.get(), [1.0, 0.0, 0.0]);
+        })
+    });
+}
+
 criterion_group!(
     benches,
     create_lut_benchmark,
     pack_benchmark,
-    unpack_benchmark
+    unpack_benchmark,
+    rustified_unpack_benchmark,
+    rustified_pack_benchmark,
 );
 criterion_main!(benches);
